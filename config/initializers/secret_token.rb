4506b16c2006a783c4b4b8622e5f204e35f1b14b
Hemant Kumar <gethemant@gmail.com>
2012-10-22 18:35:31 +0530

    Initialize new business app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MightbuyBusiness::Application.config.secret_token = '8f91362cda203abe7140dce4c983098444c1f5ba3adcfb2404e238417a1bce30d6473bfb130d38dd60e90182dfc56aa9ca6cb4916243f039edcc228cb2619614'
