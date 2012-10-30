3c4d792bc37b301748732c134e2976594a31898d
Hemant Kumar <gethemant@gmail.com>
2012-10-31 01:47:11 +0530

    Do not dragonfly for now

--- config/application.rb
+++ config/application.rb
@@ -57,11 +57,12 @@ module MightbuyBusiness
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Don't access the DB or load models when precompiling your assets.
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.1'
 
-    # Dragonfly middleware.
-    config.middleware.insert 1, 'Dragonfly::Middleware', :images
   end
 end
 
