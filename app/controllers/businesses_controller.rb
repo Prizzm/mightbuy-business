e3e4f7881656c27a428bb887b884f33dc5bf1df9
Girisha <grsh84@gmail.com>
2012-11-01 13:46:30 +0530

    Implemented Signup HTML page and CSS related to it.

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,9 +1,12 @@
 class BusinessesController < ApplicationController
-  skip_before_filter :authenticate_business_staff!, only: [:new, :create]
+  skip_before_filter :authenticate_business_staff!, only: [:new, :create, :foo]
 
-  layout 'business'
-
+  layout 'application'
+  
   def new
-
+  end
+  
+  def foo
+    render layout: 'business'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 MightbuyBusiness::Application.routes.draw do
   devise_for :business_staffs
-  resources :businesses
+  resources :businesses do
+    collection do
+      get :foo
+    end
+  end
   root :to => "welcome#index"
 end
