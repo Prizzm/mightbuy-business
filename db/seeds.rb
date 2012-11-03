6b5b17acffc9b5482f59e1ee82fcefac6f890766
Hemant Kumar <gethemant@gmail.com>
2012-11-03 13:48:36 +0530

    Add kaminari themes and real product listing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+15.times do |i|
+  Product.create!(url: "http://www.apple.com/idevise#{i}")
+end
