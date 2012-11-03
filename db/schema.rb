4f43290e59c0e4e6822538cad6f8f80925cd4b91
Hemant Kumar <gethemant@gmail.com>
2012-11-03 07:38:31 +0530

    Generate product controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121102180411) do
+ActiveRecord::Schema.define(:version => 20121103020606) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -172,6 +172,15 @@ ActiveRecord::Schema.define(:version => 20121102180411) do
     t.text     "description", :default => ""
     t.datetime "created_at",                  :null => false
     t.datetime "updated_at",                  :null => false
+  end
+
+  create_table "customer_leads", :force => true do |t|
+    t.string   "email"
+    t.string   "name"
+    t.string   "status"
+    t.integer  "product_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "deal_deals", :force => true do |t|
