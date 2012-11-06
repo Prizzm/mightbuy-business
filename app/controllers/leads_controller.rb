cb949073ca879d60df7405bf0cce55ef0ae4d9df
Yuva <yuva@codemancers.com>
2012-11-06 13:46:26 +0530

    populate leads index page with actual leads

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,8 +1,21 @@
 class LeadsController < ApplicationController
   before_filter :find_business
+  before_filter :find_lead!, only: :destroy
   layout 'business_with_sidebar'
 
   def index
+    @leads = @business.customer_leads.page(params[:page]).per(10)
+  end
 
+  def destroy
+    @lead.destroy
+    redirect_to leads_path
+  end
+
+  private
+  def find_lead!
+    unless @lead = @business.customer_leads.find_by_id(params[:id])
+      redirect_to root_path
+    end
   end
 end
