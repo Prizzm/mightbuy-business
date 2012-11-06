9d340fd80108513cca981026e7b2a73708262090
Hemant Kumar <gethemant@gmail.com>
2012-11-06 12:06:03 +0530

    remove authenticate_business_login! method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :authenticate_business_staff!, :authenticate_business_login!
+  before_filter :authenticate_business_staff!
 
   private
   def find_business
     unless @business = current_business_staff.business
       redirect_to root_path
-    end
-  end
-
-  def authenticate_business_login!
-    if session.has_key?(:retail_session)
-      sign_out
-
-      session.delete(:retail_login)
-      session.delete(:retail_session)
-      redirect_to(new_business_staff_session_path)
     end
   end
 
