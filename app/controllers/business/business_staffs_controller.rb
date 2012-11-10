aca534c85f90d3ad713b77616c195b6b61151d39
Hemant Kumar <gethemant@gmail.com>
2012-11-10 17:22:34 +0530

    Implement staff managment

--- app/controllers/business/business_staffs_controller.rb
+++ app/controllers/business/business_staffs_controller.rb
@@ -3,6 +3,20 @@ class Business::BusinessStaffsController < ApplicationController
   before_filter :find_business
 
   def index
-    @business_staffs = @business.business_staffs
+    @business_staffs = @business.business_staffs.page(params[:page]).per(10)
+    @business_staff = BusinessStaff.new()
+  end
+
+  def create
+    business_staff_params = params[:business_staff]
+    business_staff_params.update(password_confirmation: business_staff_params[:password], business: @business)
+    business_staff = BusinessStaff.create(business_staff_params)
+    if business_staff.errors.empty?
+      flash[:notice] = "Successfully added"
+    else
+      flash[:error] = business_staff.errors.full_messages.first
+    end
+    redirect_to business_business_staffs_path
   end
 end
+
