cc32ccb4f9848c41838e0d6feddf46e670c7a3c6
Charles Oppenheimer <choppen5@yahoo.com>
2012-11-14 21:47:21 -0800

    updated mailing list incentive

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121115041409) do
+ActiveRecord::Schema.define(:version => 20121115045212) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -264,6 +264,7 @@ ActiveRecord::Schema.define(:version => 20121115041409) do
     t.boolean  "ask_for_phonenumber",      :default => false
     t.boolean  "ask_for_description",      :default => false
     t.boolean  "ask_for_emergency_number", :default => false
+    t.string   "join_list_description"
   end
 
   create_table "orders", :force => true do |t|
