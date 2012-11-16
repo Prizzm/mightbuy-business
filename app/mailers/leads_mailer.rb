895e19bb80753c7e35b63d5b9026c8466d6cbaf5
Charles Oppenheimer <choppen5@yahoo.com>
2012-11-16 14:22:59 -0800

    bcc email

--- app/mailers/leads_mailer.rb
+++ app/mailers/leads_mailer.rb
@@ -1,9 +1,11 @@
 class LeadsMailer < ActionMailer::Base
+  BCC_ADDRESS = "charles@mightbuy.it".freeze
+
   def invite_customer(lead_invite)
     @lead_invite = lead_invite
 
     from    = "#{@lead_invite.business_name} <no-reply@mightbuy.it>"
     subject = "[MB] You are invited to join 'MightBuy'"
-    mail to: @lead_invite.email, from: from, subject: subject
+    mail to: @lead_invite.email, bcc: BCC_ADDRESS, from: from, subject: subject
   end
 end
