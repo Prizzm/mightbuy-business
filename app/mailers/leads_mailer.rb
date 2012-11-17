8f174a7e44ffb13cec48ea77019afaded4d1e3af
Charles Oppenheimer <choppen5@yahoo.com>
2012-11-17 10:37:33 -0800

    tried changing conent type

--- app/mailers/leads_mailer.rb
+++ app/mailers/leads_mailer.rb
@@ -3,10 +3,10 @@ class LeadsMailer < ActionMailer::Base
 
   def invite_customer(lead_invite)
     @lead_invite = lead_invite
-
+    content_type = "multipart/alternative"
     from    = "#{@lead_invite.business_name} <no-reply@mightbuy.it>"
     subject = "[MB] You are invited to join 'MightBuy'"
-    attachments.inline['storephoto'] = @lead_invite.lead.normalized_photo.url
+    attachments['storephoto'] = @lead_invite.lead.normalized_photo.url
     mail to: @lead_invite.email, bcc: BCC_ADDRESS, from: from, subject: subject
     
   end
