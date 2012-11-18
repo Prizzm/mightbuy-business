c64769cdc7b4fbd859716b15a50424c193c3c57f
Charles Oppenheimer <choppen5@yahoo.com>
2012-11-17 22:56:38 -0800

    updated image url

--- app/helpers/leads_helper.rb
+++ app/helpers/leads_helper.rb
@@ -1,5 +1,5 @@
 module LeadsHelper
   def lead_photo_url_helper(lead)
-    lead.photo ? lead.normalized_photo.thumb('100x100!').url : "/assets/no-image.gif"
+    lead.photo ? lead.photo.thumb('100x100!').url : "/assets/no-image.gif"
   end
 end
