631b70c6a80c082f4193762694e864c93235ada3
Iszak Bryan <iszak@karmarama.com>
2012-11-24 20:23:24 +0000

    Added extra parameter to pass business_staff updated test to reflect this and also new factory

--- app/mailers/leads_mailer.rb
+++ app/mailers/leads_mailer.rb
@@ -1,8 +1,10 @@
 class LeadsMailer < ActionMailer::Base
   BCC_ADDRESS = "charles@mightbuy.it".freeze
 
-  def invite_customer(lead_invite)
+  def invite_customer(lead_invite, business_staff)
     @lead_invite = lead_invite
+    @business_staff = business_staff
+
     from    = "#{@lead_invite.business_name} <no-reply@mightbuy.it>"
     subject = "Your image from #{@lead_invite.business_name}"
     #attachments.inline['storephoto'] = @lead_invite.lead.photo.jpg.url
