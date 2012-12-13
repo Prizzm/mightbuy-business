6ed01379ca7fd7cd462f3eacba51eb16a357fa17
Charles Oppenheimer <choppen5@yahoo.com>
2012-12-13 12:30:12 -0800

    matched product select to actual products

--- app/helpers/retail_leads_helper.rb
+++ app/helpers/retail_leads_helper.rb
@@ -1,6 +1,6 @@
 module RetailLeadsHelper
   def product_list_helper(business)
-    @business.all_products.collect do |product|
+    @business.products.collect do |product|
       [ product.name, product.id ]
     end
   end
