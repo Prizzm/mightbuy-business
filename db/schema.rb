7b7fdaa32bf89ff80daf17c008fad50186273766
Charles Oppenheimer <choppen5@yahoo.com>
2012-12-27 10:28:46 +0800

    adjusted deal conditions and template

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121210083104) do
+ActiveRecord::Schema.define(:version => 20121226175926) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(:version => 20121210083104) do
     t.decimal  "discount",        :precision => 6, :scale => 2
     t.string   "payment_type",                                  :default => "MightBuy"
     t.boolean  "public"
+    t.boolean  "has_conditions"
   end
 
   create_table "beta_signups", :force => true do |t|
